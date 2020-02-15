084dc19d4e52a381af88f7063433c6a0a15cc5c6
Juan Bustos <juanantoniobustos@outlook.es>
2020-02-15 18:55:31 +0100

    11. Clase Funciones - P2

--- System/Funciones.cs
+++ System/Funciones.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 
@@ -9,10 +10,31 @@ namespace Servicios//.system
     {
         public static void Logs(string nombre_archivo, string descripcion)
         {
-            string directorio = AppDomain.CurrentDomain.BaseDirectory + "logs/" +
+            try
+            {
+                string directorio = AppDomain.CurrentDomain.BaseDirectory + "logs/" +
                     DateTime.Now.Year.ToString() + "/" +
                     DateTime.Now.Month.ToString() + "/" +
                     DateTime.Now.Day.ToString();
+
+                // Si no existe el directorio, se creará
+                if (!Directory.Exists(directorio))
+                {
+                    Directory.CreateDirectory(directorio);
+                }
+
+                StreamWriter miArchivo = new StreamWriter(directorio + "/" + nombre_archivo + ".txt", true);
+
+                string cadena = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + " >>> " + descripcion;
+
+                miArchivo.WriteLine(cadena);
+                miArchivo.Close();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }            
         }
     }
 }
