eaac2bd0da2626f342e24d5ee3938939d81c4213
Juan Bustos <juanantoniobustos@outlook.es>
2020-02-17 19:31:52 +0100

    32. Convertir JSON a Clases

---
+++ system/DataJson.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Servicios
+{
+    public class DataJson
+    {
+        // Herramienta que ayuda para transformar JSONs a CSharp
+        // http://json2csharp.com/
+        // y así poder deserializar el string (que viene como JSON) y llevarlo a clase
+
+        public string deporte { get; set; }
+        public List<Equipos> equipos { get; set; }
+    }
+}
