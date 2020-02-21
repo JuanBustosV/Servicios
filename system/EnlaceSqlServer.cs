92ec95de81890994af810c93612d846931406574
Juan Bustos <juanantoniobustos@outlook.es>
2020-02-21 19:38:18 +0100

    41. Conexión a SQL Server - P1

---
+++ system/EnlaceSqlServer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Servicios//.system
+{
+    public class EnlaceSqlServer
+    {
+        private static SqlConnection conexion = null;
+
+        public static SqlConnection Conexion
+        {
+            get { return EnlaceSqlServer.conexion; }
+        }
+    }
+}
