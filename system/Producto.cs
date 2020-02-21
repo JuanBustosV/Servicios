50fbaec6be7c03b20c1ae4c32b86ecda03485790
Juan Bustos <juanantoniobustos@outlook.es>
2020-02-21 23:17:24 +0100

    50. Clase Producto

---
+++ system/Producto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Servicios//.system
+{
+    public class Producto
+    {
+        // CURSO: 50. Clase Producto
+        public int Idproduct { get; set; }
+
+        public string Nombre { get; set; }
+
+        public double Precio { get; set; }
+
+        public int Stock { get; set; }
+    }
+}
